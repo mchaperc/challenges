3bda3d4a27ac61b5b99fbf298bbcb7731f37e6ac
Matt Chastain <matthewdchastain@gmail.com>
2018-03-13 20:05:18 -0400

    completed challenge 05

--- 05/similar_tweeters.py
+++ 05/similar_tweeters.py
@@ -1,7 +1,34 @@
 import sys
+from collections import Counter
+
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+
+import spacy
+
+import usertweets
+
+
+stop_words = set(stopwords.words('english'))
+tokenizer = RegexpTokenizer(r'\w+')
+nlp = spacy.load('en')
+
+
+def tokenize_user_tweets(tweets=[]):
+    tweets = Counter(
+                    tokenizer.tokenize(
+                        ' '.join(
+                            [word 
+                                for tweet in tweets 
+                                    for word in tweet._json['text'].split(' ') 
+                                        if word.isalpha() and word.find('http') == -1 and word not in stop_words and len(word) > 2])))
+    return [word for word in tweets if tweets[word] > 1]
 
 def similar_tweeters(user1, user2):
-    pass
+    user1_tweets = nlp(' '.join(tokenize_user_tweets(usertweets.UserTweets(user1))))
+    user2_tweets = nlp(' '.join(tokenize_user_tweets(usertweets.UserTweets(user2))))
+    return user1_tweets.similarity(user2_tweets)
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
---
+++ 05/usertweets.py
@@ -0,0 +1,53 @@
+from collections import namedtuple
+import csv
+import os
+
+import tweepy
+
+from config import CONSUMER_KEY, CONSUMER_SECRET
+from config import ACCESS_TOKEN, ACCESS_SECRET
+
+DEST_DIR = 'data'
+EXT = 'csv'
+NUM_TWEETS = 100
+
+
+class UserTweets(object):
+    def __init__(self, handle='', max_id=None):
+        self.API = self.api_interface()
+        self.tweets = self.get_all_tweets_for_handle(handle, max_id)
+        self.generate_csv_from_tweets(handle=handle)
+
+    def api_interface(self):
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+        return tweepy.API(auth)
+
+    def get_all_tweets_for_handle(self, handle='', max_id=None):
+        if max_id:
+            return self.API.user_timeline(self.API.get_user(handle)._json['id'], count=NUM_TWEETS, max_id=max_id)    
+        return self.API.user_timeline(self.API.get_user(handle)._json['id'], count=NUM_TWEETS)
+
+    def generate_csv_from_tweets(self, handle=''):
+        keys = self.tweets[0]._json.keys()
+        with open(f"{DEST_DIR}/{handle}.{EXT}", 'w') as output_file:
+            dict_writer = csv.DictWriter(output_file, keys, extrasaction='ignore')
+            dict_writer.writeheader()
+            for tweet in self.tweets:
+                dict_writer.writerows([tweet._json])
+
+    def __len__(self):
+        return len(self.tweets)
+
+    def __getitem__(self, position):
+        return self.tweets[position]
+
+
+if __name__ == "__main__":
+
+    for handle in ('pybites', 'bbelderbos'):
+        print('--- {} ---'.format(handle))
+        user = UserTweets(handle)
+        for tw in user[:5]:
+            print(tw)
+        print()
